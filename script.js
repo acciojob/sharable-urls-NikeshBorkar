a5022b43cd88f9a29420de99fd8ea5fd6d018eff
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-07-29 22:58:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // your code here
+document.addEventListemer('DOMContentLoaded', function(){
+	const urlForm = document.getElementById('urlForm');
+	const heading3 = document.getElementById('url');
+
+	urlForm.addEventListemer('submit', function(){
+		const nameValue = document.getElementById('name').value;
+		const yearValue = document.getElementById('year').value;
+		const newURL = `https://localhost:8080/?$name{encodeURLComponent(nameValue)}&year{encodeURLComponent(yearValue)}`
+		heading3.textContent = newURL;
+	})
+})
